b7a3c8b5d76378fabf8f27514195784b02c1b9dd
genevill <evillacian@gmail.com>
2021-12-19 01:30:17 -0800

    Added attributes and regular expressions

--- Genevill.MVC.BugTracker/Models/BugReport.cs
+++ Genevill.MVC.BugTracker/Models/BugReport.cs
@@ -9,6 +9,7 @@ namespace Genevill.MVC.BugTracker.Models
         public int Id { get; set; }
 
 #nullable enable
+        [StringLength(1000, MinimumLength = 3), Required]
         public string? Summary { get; set; }
         
         public string? Assignee { get; set; }
@@ -16,17 +17,19 @@ namespace Genevill.MVC.BugTracker.Models
         [Display(Name = "Affected User")]
         public string? AffectedUser { get; set; }
 
+        [RegularExpression(@"\d{3}-\d{3}-\d{4}", ErrorMessage = "Please enter a contact number in the format 123-456-7890")]
         [Display(Name = "Phone Number")]
         public string? PhoneNumber { get; set; }
 
+        [RegularExpression(@"Pending|In\nProgress|Resolved|Closed", ErrorMessage = "Must be Pending, In Progress, Resolved, or Closed")]
         public string? Status { get; set; }
         
         public string? Resolution { get; set; }
-#nullable disable
-        [DataType(DataType.Date)]
-        public DateTime? Created { get; set; }
 
         [DataType(DataType.Date)]
-        public DateTime? Updated { get; set; }
+        public DateTime Created { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Updated { get; set; }
     }
 }
